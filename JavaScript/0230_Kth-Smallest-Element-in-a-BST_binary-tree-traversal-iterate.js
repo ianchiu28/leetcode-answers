7074001c05902beff2f4910cd9297bfce788041b
Ian Chiu <nk9710632@gmail.com>
2025-03-15 16:36:01 +0800

    feat: update 230

--- JavaScript/0230_Kth-Smallest-Element-in-a-BST_binary-tree-traversal-iterate.js
+++ JavaScript/0230_Kth-Smallest-Element-in-a-BST_binary-tree-traversal-iterate.js
@@ -13,20 +13,20 @@
  */
 var kthSmallest = function(root, k) {
     const stack = [];
-    let cur = root;
+    let current = root;
 
-    while (cur || stack.length > 0) {
-        if (cur) {
-            // Save cur into queue, so that we can process it later
-            stack.push(cur);
-            cur = cur.left;
-        } else {
-            const node = stack.pop();
-
-            if (k-- === 1) return node.val;
-
-            // Go to right node to ensure the in-order traversal
-            cur = node.right;
+    while (current || stack.length > 0) {
+        // Go all the way to the left
+        while (current) {
+            stack.push(current);
+            current = current.left;
         }
+
+        // Process it
+        const node = stack.pop();
+        if (k-- === 1) return node.val;
+
+        // Go to right
+        current = node.right;
     }
 };
