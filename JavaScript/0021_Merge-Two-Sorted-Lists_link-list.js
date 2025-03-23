3362fbae81fe48b7f0a06bd01395f310490744ac
Ian Chiu <nk9710632@gmail.com>
2025-03-23 21:53:41 +0800

    feat: 21

---
+++ JavaScript/0021_Merge-Two-Sorted-Lists_link-list.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    if (!list1) return list2;
+    if (!list2) return list1;
+
+    // Generate a fake node to simplify code.
+    const fakeNode = new ListNode();
+
+    // Iterate two list nodes.
+    let head = cur = fakeNode;
+    while(list1 && list2) {
+        if (list1.val < list2.val) {
+            cur.next = list1;
+            list1 = list1.next;
+        } else {
+            cur.next = list2;
+            list2 = list2.next;
+        }
+        cur = cur.next;
+    }
+
+    // Concat the rest list nodes.
+    cur.next = list1 || list2;
+
+    // Return the real list nodes.
+    return head.next;
+};
