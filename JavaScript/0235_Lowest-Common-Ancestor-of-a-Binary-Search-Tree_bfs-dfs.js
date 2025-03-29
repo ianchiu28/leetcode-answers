9089a4243bd4154b509a1c21eaeceda1bbe70f10
Ian Chiu <nk9710632@gmail.com>
2025-03-29 18:37:38 +0800

    feat:235

---
+++ JavaScript/0235_Lowest-Common-Ancestor-of-a-Binary-Search-Tree_bfs-dfs.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    while(root) {
+        if (p.val > root.val && q.val > root.val) {
+            root = root.right;
+        } else if (p.val < root.val && q.val < root.val) {
+            root = root.left
+        } else {
+            return root;
+        }
+    }
+
+    // if (p.val > root.val && q.val > root.val) {
+    //     return lowestCommonAncestor(root.right, p, q);
+    // } else if (p.val < root.val && q.val < root.val) {
+    //     return lowestCommonAncestor(root.left, p, q);
+    // } else {
+    //     return root;
+    // }
+};
